0998ba98dc2557bca8729ed865378cfa9ce41da6
frontexas <forfron@gmail.com>
2017-11-09 19:40:43 -0600

    Working on code_fights_k_palindrome: Dynamic programming

---
+++ code_fights_k_palindrome.py
@@ -0,0 +1,14 @@
+'''
+https://codefights.com/interview-practice/task/x3rJpdZGEcjmYtDqv
+
+A string is a k-palindrome if it can be transformed into a palindrome by removing any amount of characters from 0 to k. Your task is to determine whether the given string s is a k-palindrome.
+
+'''
+
+def k_palindrome_helper(s, char_removed, k, cache):
+	if is_palindrome(s):
+
+
+def k_palindrome(s, k):
+	cache = {}
+	k_palindrome_helper(s, 0, k, cache)
