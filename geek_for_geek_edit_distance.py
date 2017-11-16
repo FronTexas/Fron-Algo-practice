94e752f1baa6699811fab92fdcd154fe62ab5796
frontexas <forfron@gmail.com>
2017-11-16 12:46:55 -0600

    Add memoization to edit distance solution

--- geek_for_geek_edit_distance.py
+++ geek_for_geek_edit_distance.py
@@ -1,4 +1,7 @@
-def find_smallest_edit_distance(s1,s2):
+def find_smallest_edit_distance(s1,s2, cache):
+	
+	if (s1, s2) in cache[(s1, s2)]: return cache[(s1, s2)]
+
 	head_1, head_2 = 0, 0 
 	tail_1, tail_2 = len(s1) - 1, len(s2) - 1
 
@@ -14,9 +17,11 @@ def find_smallest_edit_distance(s1,s2):
 
 	if tail_1 == 0 and tail_2 == 0: return 0
 
-	insert = find_smallest_edit_distance(s2[0] + s1, s2)
-	remove
-	replace 
+	insert = min(find_smallest_edit_distance(s2[0] + s1, s2), find_smallest_edit_distance(s1 + s2[-1], s2))
+	remove = min(find_smallest_edit_distance(s1[head_1 + 1:], s2), find_smallest_edit_distance(s1[:tail_1], s2))
+	replace = min(find_smallest_edit_distance(replace(s1, 0, s2[0]), s2), find_smallest_edit_distance(replace(s1, len(s1) - 1, s2[-1]), s2))
+
+	cache[(s1, s2)] = min(insert, remove, replace)
 
 	return min(insert, remove, replace)
 
