7ac44a738474ce1325c4e97f6be3df270bc4334d
frontexas <forfron@gmail.com>
2017-11-24 18:06:43 -0600

    Refactor group dishes solution again

--- group_dishes_code_fights.py
+++ group_dishes_code_fights.py
@@ -7,17 +7,19 @@ class IngridientsToDishDict:
 	def __init__(self):
 		self.ingridients_to_dish = {}
 
-	def add_new_mappings_between_ingridient_and_dish_name(self, ingridients, dish_name):
-		for ingridient in ingridients: 
-			if ingridient in self.ingridients_to_dish:
+	def add_if_ingridient_in_dict_create_new_array_otherwise(self, ingridient, dish_name):
+		if ingridient in self.ingridients_to_dish:
 				self.ingridients_to_dish[ingridient].append(dish_name)
 				self.ingridients_to_dish[ingridient].sort()
-			else: 
-				self.ingridients_to_dish[ingridient] = [dish_name]
+		else: 
+			self.ingridients_to_dish[ingridient] = [dish_name]
+
+	def add_new_mappings_between_ingridient_and_dish_name(self, ingridients, dish_name):
+		for ingridient in ingridients: 
+			self.add_if_ingridient_in_dict_create_new_array_otherwise(ingridient, dish_name)
 	
 	def get_dictionary(self):
 		return self.ingridients_to_dish
-
 
 class DictToArrayService: 
 	def convert_dict_to_2d_array(self, d):
@@ -40,8 +42,6 @@ def groupingDishes(dishes):
 
 	dishes_grouping_array = DictToArrayService().convert_dict_to_2d_array(ingridients_to_dish.get_dictionary())
 	return sorted(remove_from_array_if_length_less_than_three(dishes_grouping_array))
-
-
 
 dishes = [["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
             ["Pizza", "Tomato", "Sausage", "Sauce", "Dough"],
