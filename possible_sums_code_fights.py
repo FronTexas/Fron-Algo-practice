697fbb4a4b473fe38b1f5af7f97477f2149d6f00
frontexas <forfron@gmail.com>
2017-11-29 23:46:22 -0600

    Refactor possible sum solution

--- possible_sums_code_fights.py
+++ possible_sums_code_fights.py
@@ -1,21 +1,30 @@
 # https://codefights.com/interview-practice/task/rMe9ypPJkXgk3MHhZ
 
-def possibleSumRecursive(unique_sum, sum_so_far, coin_to_quantity):
-	if sum_so_far > 0:
-		unique_sum.add(sum_so_far)
-	for coin in coin_to_quantity:
-		if coin_to_quantity[coin] > 0: 
-			coin_to_quantity[coin] -= 1
-			possibleSumRecursive( unique_sum, sum_so_far + coin, coin_to_quantity)
-			coin_to_quantity[coin] += 1
+class uniqueSum: 
+	def __init__(self):
+		self.unique_sum = set()
+	
+	def build_all_possible_sum(self, coin_to_quantity):
+		self.build_all_possible_sum_recursive(0, coin_to_quantity)
 
+	def build_all_possible_sum_recursive(self, sum_so_far, coin_to_quantity):
+		for coin in coin_to_quantity:
+			if coin_to_quantity[coin] > 0: 
+				coin_to_quantity[coin] -= 1
+				if sum_so_far + coin > 0:
+					self.unique_sum.add(sum_so_far + coin)
+				self.build_all_possible_sum_recursive(sum_so_far + coin, coin_to_quantity)
+				coin_to_quantity[coin] += 1
+
+	def get(self):
+		return self.unique_sum
 
 
 
 def possibleSums(coins, quantity):
 	coin_to_quantity = {coins[i] : quantity[i] for i in range(len(coins))}
-	unique_sum = set()
-	possibleSumRecursive(unique_sum, 0, coin_to_quantity)
-	return len(unique_sum)
+	unique_sum = uniqueSum()
+	unique_sum.build_all_possible_sum(coin_to_quantity)
+	return len(unique_sum.get())
 
 print possibleSums([10,50,100],[1,2,1])
