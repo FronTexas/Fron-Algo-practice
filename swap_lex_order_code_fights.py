fa90514afe671f2e42ee856f8fd30e0b0822d20f
frontexas <forfron@gmail.com>
2017-12-08 09:58:23 -0600

    Start implement build_connected_pairs

--- swap_lex_order_code_fights.py
+++ swap_lex_order_code_fights.py
@@ -8,28 +8,39 @@
 '''
 
 def swapLexOrder(str, pairs):
-	considered_strings = set([])
-	stack = [str]
-	string_with_maximum_lex = str
 	pairs = [[i-1, j-1] for i, j in pairs]
+	connected_pairs = build_connected_pairs(pairs)
 
-	while len(stack) > 0: 
-		current_string = stack.pop()
-		if current_string not in considered_strings:
-			considered_strings.add(current_string)
-			for pair in pairs: 
-				i, j = pair
-				next_string = swap(i, j, current_string)
-				if next_string not in considered_strings:
-					stack.append(next_string)
-					# string_with_maximum_lex = max(string_with_maximum_lex, next_string)
-	return string_with_maximum_lex
 
-def swap(i, j, string):
-	modified_string = [c for c in string]
-	modified_string[i], modified_string[j] = modified_string[j], modified_string[i]
-	return ''.join(modified_string)
- 
-print swapLexOrder('abdc', [[1, 4], [3, 4]])
-print swapLexOrder('a', [])
+def build_connected_pairs_recursive(connected_pair_so_far, pairs, memo, connected_pairs):
+	for pair in pairs: 
+		if tuple(pair) not in memo and (len(connected_pair_so_far) == 0 or has_connection(connected_pair_so_far, pair)):
+			memo.add(tuple(pair))
+			build_connected_pairs_recursive(build_union(connected_pair_so_far, pair), pairs, memo, connected_pairs)
+	
+	if len(connected_pair_so_far) > 0: 
+		connected_pairs.append(connected_pair_so_far)
+
+def has_connection(list1, list2):
+	return len(list(set(list1) & set(list2))) > 0 
+
+def build_union(list1, list2):
+	return list(set(list1) | set(list2))
+
+def build_connected_pairs(pairs):
+	connected_pairs = []
+	build_connected_pairs_recursive([], pairs, set([]), connected_pairs)
+	return connected_pairs
+
+def assertExpectedEqualsActual(test_name, expected, actual): 
+	if expected != actual: 
+		print '*** FAILED ***'
+		print test_name
+		print 'expected = ', expected 
+		print 'actual = ', actual
+
+print assertExpectedEqualsActual('build_connected_pairs',[[1,3,4]], build_connected_pairs([[1, 4], [3, 4]]))
+# print assertExpectedEqualsActual('swapLexOrder', 'dbca', swapLexOrder('abdc', [[1, 4], [3, 4]]))
+# print assertExpectedEqualsActual('swapLexOrder', 'a', swapLexOrder('a', []))
+
 
